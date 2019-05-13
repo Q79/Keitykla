e1393bbfdc558cda6466db8968d3f61a9bbd7d0e
Justina <justina.bauziene@gmail.com>
2019-05-13 21:23:30 +0300

    +

--- Keitykla/Keitykla/Program.cs
+++ Keitykla/Keitykla/Program.cs
@@ -10,7 +10,14 @@ namespace Keitykla
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("I ka noretumete konvertuoti:[1] USD, [2] PLN");
+            string valiuta = "";
+            Console.WriteLine("Iveskite EUR kieki:");
+            decimal keiciamaSuma = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Jums priklauso: {0} {1}", Keisti(valiuta, keiciamaSuma), valiuta);
+
+
+
         }
         static void double Keisti (int valiuta, decimal keiciamaSuma)
         {
